b73794a926d177202e0815787901821ca248e8d4
Vinicius Oliveira <viniciusos@take.net>
2020-03-25 20:13:32 -0300

    Adicionando componente card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaButtonComponent } from './ma-button/ma-button.component';
+import { MaCardComponent } from './ma-card/ma-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MaButtonComponent,
+    MaCardComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/ma-card/ma-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ma-card',
+  templateUrl: './ma-card.component.html',
+  styleUrls: ['./ma-card.component.scss']
+})
+export class MaCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
